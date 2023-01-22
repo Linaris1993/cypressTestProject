54a5782156f950b4b8449eabdc537ae3be2cbb6e
Linaris1993 <linakhatmullina@gmail.com>
2023-01-21 17:28:33 -0800

    added containst tests

--- cypress/e2e/firstTest.spec.cy.js
+++ cypress/e2e/firstTest.spec.cy.js
@@ -1,1 +1,22 @@
 /// <reference types="Cypress" />
+//contains used mostly for text search (buttons, links and etc)
+
+it('look for sigh-in btn', () => {
+    cy.visit("https://next.privat24.ua/mobile?lang=en");
+    cy.contains('Sign in');
+});
+
+it('look tag with sign-in text', () => {
+    cy.visit("https://next.privat24.ua/mobile?lang=en");
+    cy.contains('div', 'Sign in');
+});
+
+it('look for text not case sensitive', () => {
+    cy.visit("https://next.privat24.ua/mobile?lang=en");
+    cy.contains('SIGN IN', {matchCase: false}); //will find sign in with small letters
+});
+
+it.only('look for text not case sensitive', () => {
+    cy.visit("https://next.privat24.ua/mobile?lang=en");
+    cy.get('footer').contains('Go to old version');
+});
